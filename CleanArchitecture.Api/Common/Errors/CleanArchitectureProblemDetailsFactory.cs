d0b992e48f02244f12e4d68311d24317b2a30e5e
Faraz Ali <faraz.ali@greenlightlabs.tech>
2022-09-22 16:45:27 +0500

    Error controller added

---
+++ CleanArchitecture.Api/Common/Errors/CleanArchitectureProblemDetailsFactory.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace CleanArchitecture.Api.Common.Errors;
+
+public class CleanArchitectureProblemDetailsFactory
+{
+
+}
--- CleanArchitecture.Api/Program.cs
+++ CleanArchitecture.Api/Program.cs
@@ -1,5 +1,7 @@
+using CleanArchitecture.Api.Common.Errors;
 using CleanArchitecture.Application;
 using CleanArchitecture.Infastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -7,10 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfastructure(builder.Configuration);
     builder.Services.AddControllers();
+
+    //builder.Services.AddSingleton<ProblemDetailsFactory, CleanArchitectureProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
+
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
